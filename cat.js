53ef835ea612ed848841dde4dd6677b1c19aa497
Cassandra <cjames21082@gmail.com>
2013-07-08 14:48:42 -0700

    Fancy cat features

--- cat.js
+++ cat.js
@@ -10,25 +10,42 @@ var cat = {
 	sleep: function(mins) {
 		for (var i=0; i<mins; i++) {
 			console.log('z');
-			this.tiredness--;
+			this.tiredness-= 2;
 		}
 	},
-	
-    eat: function() {
+    eat: function(food) {
+		if (food ==="tuna"){
+			console.log('mmmm.tuna');
+			this.hunger -=5;
+		} else if (food === "dog food"){
+			console.log("yuck! I can't stand this crap.");
+			this.hunger +=3;
+			this.happiness -=2;
+		} else{
 		console.log('chomp');
 		this.hunger--;
-	},
-
-	play: function(mins) {
-		for (var i=0; i<mins; i++) {
-			console.log('whee!');
-			this.happiness++;
-			this.loneliness--;
-			this.tiredness++;
-			this.hunger++;
 		}
 	},
-    
+
+	play: function(mins, type) {
+		for (var i=0; i<mins; i++) {
+			if (type === "chasing mice"){
+				console.log("I like to catch mice!!");
+				this.happiness += 2;
+				this.tiredness +=2;
+			} else if (type === "scratching post"){
+				console.log("scratching a post. whee!");
+				this.loneliness++;
+				this.hunger += 3;
+			} else{
+				console.log('whee!');
+				this.happiness++;
+				this.loneliness--;
+				this.tiredness++;
+				this.hunger++;
+			}
+		}
+	},
 	status: function() {
 		console.log("tiredness; " + this.tiredness);
 		console.log("hunger: " + this.hunger);
